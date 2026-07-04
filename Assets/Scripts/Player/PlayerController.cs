fcffd3e77bf0a73e42d72f2e3a16d878ca25f57a
{Anshul <{anshulgurjar852@gmail.com}>
2026-07-04 23:08:16 +0530

    feat: implement basic player movement

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float moveSpeed = 5f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Debug.Log("Horizontal: " + horizontal + " Vertical: " + vertical);
+        Vector3 movement = new Vector3(horizontal , 0f , vertical);
+        transform.Translate(movement * moveSpeed * Time.deltaTime);
+    }
+}
