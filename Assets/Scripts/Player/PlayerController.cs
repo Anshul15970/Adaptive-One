df95cf9013e1d0fe924002d312a78d9ddcc6cac1
{Anshul <{anshulgurjar852@gmail.com}>
2026-07-05 21:36:13 +0530

    refactor: migrate player movement to Character controller

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,9 +4,10 @@ public class PlayerController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public float moveSpeed = 5f;
+    private CharacterController controller;
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
@@ -16,6 +17,6 @@ public class PlayerController : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
         Debug.Log("Horizontal: " + horizontal + " Vertical: " + vertical);
         Vector3 movement = new Vector3(horizontal , 0f , vertical);
-        transform.Translate(movement * moveSpeed * Time.deltaTime);
+        controller.Move(movement * moveSpeed * Time.deltaTime);
     }
 }
